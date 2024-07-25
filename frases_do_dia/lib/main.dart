fa38f5f9937bd07d21ed7157d2a69d2a0f5fd1cc
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2024-07-24 22:44:56 -0300

    Criação da tela + lógiaca para geração das frases

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,9 +1,78 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Column()
+  runApp( MaterialApp(
+      home: Home()
+  )
   );
 }
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var _texto = ["teste1","teste2", "teste3","teste4","teste5"];
+  var _textoAtual = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen,
+        title: Text(
+            "Frases do dia",
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+             Image.asset(
+                  "imgs/logo.png",
+                ),
+            Text(
+                _textoAtual,
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+             ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      var rng = Random();
+                      _textoAtual = _texto[rng.nextInt(5)];
+                    });
+                  },
+                  child: Text("Nova frase"),
+                  style: ElevatedButton.styleFrom(
+                      shape: BeveledRectangleBorder(),
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                      backgroundColor: Colors.lightGreen
+                  )
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 
